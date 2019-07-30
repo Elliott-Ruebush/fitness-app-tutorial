5014e7746162eeaaca95e2ff297c965cd67ca08b
Elliott Ruebush <eruebush@gmail.com>
2019-07-29 22:47:19 -0400

    Added TextFields for title, description, and category of exercise to the create exercise dialog

--- src/Components/Exercises/Dialogs/Create.js
+++ src/Components/Exercises/Dialogs/Create.js
@@ -1,9 +1,36 @@
-import React, { Fragment } from 'react';
-import { Button, Dialog, DialogContent, DialogTitle, DialogActions, DialogContentText, Icon, Fab } from '@material-ui/core';
+import React, { Fragment, useState } from 'react';
+import {
+    Button,
+    Dialog,
+    DialogContent,
+    DialogTitle,
+    DialogActions,
+    DialogContentText,
+    Icon,
+    Fab,
+    TextField,
+    MenuItem
+} from '@material-ui/core';
 import AddIcon from '@material-ui/icons/Add';
 
 export default function FormDialog() {
+    //TODO: Bring in the muscles list from App.js instead of recreating the same list in the Create component
+    const muscleList = [
+        'Back',
+        'Legs',
+        'Arms',
+        'Chest',
+        'Shoulders'
+    ];
+
     const [open, setOpen] = React.useState(false);
+    const [newEx, setNewEx] = React.useState({
+        'id': '',
+        title: '',
+        description: '',
+        muscles: ''
+
+    })
 
     function handleClickOpen() {
         setOpen(true);
@@ -11,6 +38,10 @@ export default function FormDialog() {
 
     function handleClose() {
         setOpen(false);
+    }
+
+    const handleChange = name => event => {
+        setNewEx({ ...newEx, [name]: event.target.value });
     }
 
     return (
@@ -30,14 +61,53 @@ export default function FormDialog() {
                         Fill out the form to add an exercise
                 </DialogContentText>
                     <form>
-
+                        <TextField
+                            id="set-title"
+                            label="Exercise Title"
+                            // className={classes.textField}
+                            value={newEx.title}
+                            onChange={handleChange('title')}
+                            margin="normal"
+                        />
+                        <br />
+                        <TextField
+                            id="set-desc"
+                            multiline
+                            rows="3"
+                            label="Exercise Description"
+                            value={newEx.description}
+                            onChange={handleChange('description')}
+                            margin="normal"
+                        />
+                        <br />
+                        <TextField
+                            id="select-category"
+                            select
+                            label="Select Muscle"
+                            // className={classes.textField}
+                            value={newEx.muscles}
+                            onChange={handleChange('muscles')}
+                            // SelectProps={{
+                            //     MenuProps: {
+                            //         className: classes.menu,
+                            //     },
+                            // }}
+                            helperText="Please give muscle category"
+                            margin="normal"
+                        >
+                            {muscleList.map(option => (
+                                <MenuItem key={option} value={option.toLowerCase()}>
+                                    {option}
+                                </MenuItem>
+                            ))}
+                        </TextField>
                     </form>
                 </DialogContent>
                 <DialogActions>
-                    <Button onClick={handleClose} color="inherit" variant="raised">
+                    <Button onClick={handleClose} color="inherit">
                         Cancel
           </Button>
-                    <Button onClick={handleClose} color="inherit" variant="raised">
+                    <Button onClick={handleClose} color="inherit">
                         Add
           </Button>
                 </DialogActions>
